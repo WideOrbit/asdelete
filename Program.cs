3a82501c3925684acdc4a040d2e2da53df83c77d
Per Jahn <perjahn@gmail.com>
2017-02-03 12:23:13 +0100

    Added support for regex matching set names. Added support for delete using low ttl.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,10 +15,11 @@ namespace asdelete
         static AerospikeClient _client;
         static long _count, _rewritecount, _total;
         static int _thresholdTime;
+        static string _deleteSets;
         static long _deleteLimit;
         static long _deleteRangeStart;
         static long _deleteRangeEnd;
-        static bool _verbose;
+        static bool _verbose, _rewrite;
         static Stopwatch _watch;
 
         static int Main(string[] args)
@@ -30,6 +32,16 @@ namespace asdelete
             else
             {
                 _verbose = false;
+            }
+
+            if (args.Contains("-userewrite"))
+            {
+                _rewrite = true;
+                args = args.Where(a => a != "-userewrite").ToArray();
+            }
+            else
+            {
+                _rewrite = false;
             }
 
             int port, days;
@@ -45,16 +57,17 @@ namespace asdelete
                 Console.WriteLine(
 @"Tool for pre-empty deletion of Aerospike objects that will soon be deleted due to their TTL.
 
-Version 1.2
+Version 1.3
 
-Usage: asdelete [-verbose] <host> <port> <namespace> <set> <days> <limit> [rangestart] [rangeend]
+Usage: asdelete [-userewrite] [-verbose] <host> <port> <namespace> <sets> <days> <limit> [rangestart] [rangeend]
 
+-userewrite Delete by setting a very low TTL on object.
 -verbose:   Print expiration time for all objects that are deleted.
 
 host:       Aerospike server.
 port:       Aeropsike port.
 namespace:  Aeropsike namespace.
-set:        Aeropsike data set name.
+sets:       Aeropsike data set names (comma separated regex expressions).
 days:       Days into the future - should be a *positive* integer.
 limit:      Maximum number of objects to delete.Specify 0 to just perform a count.
 rangestart: Lower bound of date range (exclusive). Optional.
@@ -67,9 +80,9 @@ rangeend:   Upper bound of date range (inclusive). Optional.");
             return 0;
         }
 
-        static void Delete(string host, int port, string asnamespace, string set, int days, long limit, long rangeStart, long rangeEnd)
+        static void Delete(string host, int port, string asnamespace, string sets, int days, long limit, long rangeStart, long rangeEnd)
         {
-            Console.WriteLine($"Host: {host}, Port: {port}, Namespace: {asnamespace}, Set: {set}, Days: {days}, Limit: {limit}, RangeStart: {rangeStart}, RangeEnd: {rangeEnd}");
+            Console.WriteLine($"Host: {host}, Port: {port}, Namespace: {asnamespace}, Sets: {sets}, Days: {days}, Limit: {limit}, RangeStart: {rangeStart}, RangeEnd: {rangeEnd}");
 
             DateTime dt = DateTime.UtcNow.AddDays(days);
             Console.WriteLine($"Date: {dt}");
@@ -77,6 +90,7 @@ rangeend:   Upper bound of date range (inclusive). Optional.");
             _thresholdTime = UtcDateTimeToASTime(dt);
 
 
+            _deleteSets = sets;
             _deleteLimit = limit;
             _deleteRangeStart = rangeStart;
             _deleteRangeEnd = rangeEnd;
@@ -91,10 +105,10 @@ rangeend:   Upper bound of date range (inclusive). Optional.");
                 // in the cluster and return the record Digest to the call back object
 
                 _watch = Stopwatch.StartNew();
-                _client.ScanAll(scanPolicy, asnamespace, set, ScanCallback, new string[] { });
+                _client.ScanAll(scanPolicy, asnamespace, null, ScanCallback, new string[] { });
                 _watch.Stop();
 
-                Log($"Deleted {_count} records from set {set}. Rewrites: {_rewritecount}");
+                Log($"Deleted {_count} records from set {sets}. Rewrites: {_rewritecount}");
             }
             catch (AerospikeException ex)
             {
@@ -117,33 +131,20 @@ rangeend:   Upper bound of date range (inclusive). Optional.");
             {
                 if (_count < _deleteLimit)
                 {
-                    if (_verbose)
-                    {
-                        Log("Expiration: " + ASTimeToUtcDateTime(record.expiration));
-                    }
-
                     try
                     {
-                        //var firstbinkey = record.bins.Keys.FirstOrDefault();
-                        string firstbinkey = null;
-                        if (firstbinkey == null)
+                        if (!_deleteSets.Split(',').Any(s => Regex.IsMatch(key.setName, s)))
                         {
-                            if (_verbose)
-                            {
-                                Log("Using delete.");
-                            }
-                            _client.Delete(new WritePolicy(), key);
+                            return;
+                        }
+
+                        if (_rewrite)
+                        {
+                            DeleteUsingRewrite(key, record);
                         }
                         else
                         {
-                            if (_verbose)
-                            {
-                                Log("Using rewrite.");
-                            }
-                            WritePolicy wp = new WritePolicy();
-                            wp.expiration = 1;
-                            record.bins[firstbinkey] = record.bins[firstbinkey];
-                            _rewritecount++;
+                            DeleteUsingExplicitDelete(key, record);
                         }
                     }
                     catch (AerospikeException ex)
@@ -162,6 +163,34 @@ rangeend:   Upper bound of date range (inclusive). Optional.");
                     _watch = Stopwatch.StartNew();
                 }
             }
+        }
+
+        static void DeleteUsingExplicitDelete(Key key, Record record)
+        {
+            if (_verbose)
+            {
+                Log($"Expiration: {ASTimeToUtcDateTime(record.expiration)}, Using delete, Set: {key.setName}.");
+            }
+            _client.Delete(new WritePolicy(), key);
+        }
+
+        static void DeleteUsingRewrite(Key key, Record record)
+        {
+            var firstbinkey = record.bins.Keys.FirstOrDefault();
+            if (firstbinkey == null)
+            {
+                DeleteUsingExplicitDelete(key, record);
+                return;
+            }
+
+            if (_verbose)
+            {
+                Log($"Expiration: {ASTimeToUtcDateTime(record.expiration)}, Using rewrite, Set: {key.setName}.");
+            }
+            WritePolicy wp = new WritePolicy();
+            wp.expiration = 1;
+            record.bins[firstbinkey] = record.bins[firstbinkey];
+            _rewritecount++;
         }
 
         static int UtcDateTimeToASTime(DateTime dt)
